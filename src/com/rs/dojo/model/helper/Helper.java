c81db21a96521162eda118edc2dff42362c644dd
Jorge Oleques <joleques@gmail.com>
2014-01-23 09:22:50 -0200

    ajustando para aumentar o refactory

--- src/com/rs/dojo/model/helper/Helper.java
+++ src/com/rs/dojo/model/helper/Helper.java
@@ -7,48 +7,35 @@ public class Helper {
 	public static Integer getLength(final String name) {
 
 		if (name.indexOf('.') > 0) {
-			String entity = getEntity(name);
+			String entity = name.substring(0, name.indexOf('.'));
+			entity = entity.substring(0, 1).toUpperCase().concat(entity.substring(1));
 			String field = name.substring(name.indexOf('.') + 1);
 
 			try {
-				final Class<?> T = getClass(entity);
+				final Class<?> T = Class.forName("com.rs.dojo.model.helper.".concat(entity));
 				final Length length = T.getDeclaredField(field).getAnnotation(Length.class);
 				return (length != null) ? length.max() : null;
 			} catch (Exception e) {
 				// just ignore
 			}
 		}
-
 		return 0;
-	}
-
-	
-
-	private static Class<?> getClass(String entity) throws ClassNotFoundException {
-		final Class<?> T = Class.forName("com.rs.dojo.model.helper.".concat(entity));
-		return T;
-	}
-
-	private static String getEntity(final String name) {
-		String entity = name.substring(0, name.indexOf('.'));
-		entity = entity.substring(0, 1).toUpperCase().concat(entity.substring(1));
-		return entity;
-	}
+	}	
 
 	public static String getLabel(final String name) {
 		if (name.indexOf('.') > 0) {
-			String entity = getEntity(name);
+			String entity = name.substring(0, name.indexOf('.'));
+			entity = entity.substring(0, 1).toUpperCase().concat(entity.substring(1));
 			String field = name.substring(name.indexOf('.') + 1);
 
 			try {
-				final Class<?> T = getClass(entity);
+				final Class<?> T = Class.forName("com.rs.dojo.model.helper.".concat(entity));
 				final Label label = T.getDeclaredField(field).getAnnotation(Label.class);
 				return label.value();
 			} catch (Exception e) {
 				// just ignore
 			}
 		}
-
 		return null;
 	}
 
@@ -69,18 +56,18 @@ public class Helper {
 
 	public static String getViewTag(String name) {
 		if (name.indexOf('.') > 0) {
-			String entity = getEntity(name);
+			String entity = name.substring(0, name.indexOf('.'));
+			entity = entity.substring(0, 1).toUpperCase().concat(entity.substring(1));
 			String field = name.substring(name.indexOf('.') + 1);
 
 			try {
-				final Class<?> T = getClass(entity);
+				final Class<?> T = Class.forName("com.rs.dojo.model.helper.".concat(entity));
 				final ViewTag viewTag = T.getDeclaredField(field).getAnnotation(ViewTag.class);
 				return viewTag.value();
 			} catch (Exception e) {
 				// just ignore
 			}
 		}
-
 		return null;
 	}
 }
